aa74e7c91c8155766ae08293cebc841378cfecbc
SOME1HING <yashprakash2005@gmail.com>
2022-10-23 12:19:58 +0530

    hmm

--- ShikimoriMusic/plugins/info.py
+++ ShikimoriMusic/plugins/info.py
@@ -6,6 +6,12 @@ from pyrogram.types import InlineKeyboardMarkup, InlineKeyboardButton, Message
 from pyrogram.errors import UserNotParticipant
 from pyrogram import Client
 from telethon import events
+from telethon import *
+from telethon.tl.functions.account import *
+from telethon.tl.functions.channels import *
+from telethon.tl.functions.photos import *
+from telethon.tl.types import *
+from html import *
 
 from ShikimoriMusic import ASS_USERNAME, BOT_ID, ASS_NAME, ASS_ID, BOT_NAME, BOT_USERNAME, pbot, tbot
 from ShikimoriMusic.setup.filters import command
@@ -46,11 +52,9 @@ async def PPScmd(event):
         await tbot.send_message(event.chat.id, "1")
         user = await event.get_reply_message()
         await tbot.send_message(event.chat.id, "2")
-        if user:
-            photos = await event.client.get_profile_photos(user.sender)
-        else:
-            photos = await event.client.get_profile_photos(event.chat_id)
-        await tbot.send_message(event.chat.id, "3")
+        
+        photos = await event.client.get_profile_photos(user.sender)
+
         try:
             await tbot.send_photo(event.chat.id, photos, caption="hmm") 
             pic = await generate_cover(user.sender, photos)
@@ -61,3 +65,39 @@ async def PPScmd(event):
         await tbot.send_photo(event.chat.id, pic, caption="hmm") 
     except:
         await tbot.send_message(event.chat.id, "ERROR")
+        
+
+
+@tbot.on(events.NewMessage(pattern="^[!/]pfp$"))
+async def PPScmd(event):
+#        """Gets the profile photos of replied users, channels or chats"""
+    try:
+        id = "".join(event.raw_text.split(maxsplit=2)[1:]) 
+        user = await event.get_reply_message()
+        if user:
+            photos = await event.client.get_profile_photos(user.sender)
+        else:
+            photos = await event.client.get_profile_photos(event.chat_id)
+        if id.strip() == "":
+            try:
+                await event.client.send_file(event.chat.id, photos)
+            except:
+                photo = await event.client.download_profile_photo(event.chat_id)
+                await tbot.send_file(event.chat.id, photo)
+        else:
+            try:
+                id = int(id)
+                if id <= 0:
+                    await event.edit("<code>ID number you entered is invalid</code>")
+                    return
+            except:
+                await event.edit("<code>ID number you entered is invalid</code>")
+                return
+            if int(id) <= (len(photos)):
+                send_photos = await event.client.download_media(photos[id - 1])
+                await tbot.send_file(event.chat.id, send_photos)
+            else:
+                await event.edit("<code>No photo found with that id</code>")
+                return
+    except:
+        await tbot.send_message("Reply to user mate")
